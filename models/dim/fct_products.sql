5e46e8f074d51e3f4b7fec4fcf92b1a4c031e4c5
HunBug <akos.strack+github@gmail.com>
2023-10-16 14:30:59 +0200

    add avg product price (#14)
    add exposure products
    
    Co-authored-by: Ákos Strack <akos.strack@gmail.com>

--- models/dim/fct_products.sql
+++ models/dim/fct_products.sql
@@ -3,6 +3,7 @@ with product_agg as (
         product_id,
         max(unit_price) as max_unit_price,
         min(unit_price) as min_unit_price,
+        avg(unit_price) as avg_unit_price,
         count(distinct supplier_id) as suppliers_count
     from {{ ref('snp_products') }}
     group by product_id
@@ -12,9 +13,11 @@ transform as (
     select
         stg_products.product_id as product_id,
         stg_products.supplier_id as supplier_id,
+        product_name,
         unit_price as latest_unit_price,
         max_unit_price,
         min_unit_price,
+        avg_unit_price,
         suppliers_count
     from {{ ref('stg_products') }}
     left join product_agg on product_agg.product_id = stg_products.product_id 
@@ -26,8 +29,10 @@ select
     {{ dbt_utils.generate_surrogate_key(['supplier_id']) }} as supplier_fk,
     product_id,
     supplier_id,
+    product_name,
     latest_unit_price,
     max_unit_price,
     min_unit_price,
+    avg_unit_price,
     suppliers_count
 from transform
